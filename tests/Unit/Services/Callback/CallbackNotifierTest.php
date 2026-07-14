fbc8605a4206f97e9dae8555b24b84d79526b80b
Bruno Santos <bruno@brunoweblink.com>
2026-07-14 18:49:12 -0300

    feat(callback): CallbackNotifier generico substitui WebhookDownloadClient

---
+++ tests/Unit/Services/Callback/CallbackNotifierTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Services\Callback\CallbackNotifier;
+use App\Services\Callback\CallbackPermanentException;
+use Illuminate\Support\Facades\Http;
+
+it('envia POST com X-API-Token e payload', function () {
+    Http::fake(['*' => Http::response('', 200)]);
+
+    (new CallbackNotifier())->notificar('https://c.exemplo/webhook', 'tok-1', ['status' => 'concluido']);
+
+    Http::assertSent(function ($request) {
+        return $request->url() === 'https://c.exemplo/webhook'
+            && $request->hasHeader('X-API-Token', 'tok-1')
+            && $request['status'] === 'concluido';
+    });
+});
+
+it('lança permanente em 4xx', function () {
+    Http::fake(['*' => Http::response('rejeitado', 422)]);
+
+    (new CallbackNotifier())->notificar('https://c.exemplo/webhook', 'tok-1', []);
+})->throws(CallbackPermanentException::class);
+
+it('relança em 5xx', function () {
+    Http::fake(['*' => Http::response('erro', 500)]);
+
+    (new CallbackNotifier())->notificar('https://c.exemplo/webhook', 'tok-1', []);
+})->throws(\Illuminate\Http\Client\RequestException::class);
